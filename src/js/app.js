1e3a4105499057927433d21b8590536e82be70ae
Ruslan Timurziyev <ruslan.timurziyev@gmail.com>
2017-09-04 23:22:08 +0600

    +

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 // Desctop icons
 new IconComponent({ name: 'Другое',img: 'folder', entry: '#app', ID: 'another-files' });
 new IconComponent({ name: 'Мои документы',img: 'folder', entry: '#app', ID: 'my-files' });
+new IconComponent({ name: 'Много файлов',img: 'folder', entry: '#app', ID: 'many-files' });
 
 
 // Windows
@@ -8,14 +9,18 @@ new IconComponent({ name: 'Мои документы',img: 'folder', entry: '#ap
 var anotherFiles = new WindowComponent({
 	action: '#another-files',
 	ID: 'anotherFiles',
-})
+});
 var myFiles = new WindowComponent({
 	action: '#my-files',
-	ID: 'myCompWindow',
+	ID: 'myFiles',
+});
+var manyFiles = new WindowComponent({
+	action: '#many-files',
+	ID: 'manyFiles',
 });
 
 
-// Windows files
+// Data
 anotherFiles.files = [
 	new IconComponent({ name: 'Текстовый файл 1', img: 'text-file', entry: '#anotherFiles' }),
 	new IconComponent({ name: 'Текстовый файл 2', img: 'text-file', entry: '#anotherFiles' }),
@@ -23,16 +28,31 @@ anotherFiles.files = [
 	new IconComponent({ name: 'Текстовый файл 4', img: 'text-file', entry: '#anotherFiles' }),
 ];
 myFiles.files = [
-	new IconComponent({ name: 'Текстовый файл 1', img: 'text-file', entry: '#myCompWindow' }),
-	new IconComponent({ name: 'Текстовый файл 2', img: 'text-file', entry: '#myCompWindow' }),
-	new IconComponent({ name: 'Текстовый файл 3', img: 'text-file', entry: '#myCompWindow' }),
-	new IconComponent({ name: 'Текстовый файл 4', img: 'text-file', entry: '#myCompWindow' }),
-	new IconComponent({ name: 'Видео-файл 1', img: 'movie', entry: '#myCompWindow' }),
-	new IconComponent({ name: 'Видео-файл 2', img: 'movie', entry: '#myCompWindow' }),
-	new IconComponent({ name: 'Видео-файл 3', img: 'movie', entry: '#myCompWindow' }),
-	new IconComponent({ name: 'Видео-файл 4', img: 'movie', entry: '#myCompWindow' }),
+	new IconComponent({ name: 'Текстовый файл 1', img: 'text-file', entry: '#myFiles' }),
+	new IconComponent({ name: 'Текстовый файл 2', img: 'text-file', entry: '#myFiles' }),
+	new IconComponent({ name: 'Текстовый файл 3', img: 'text-file', entry: '#myFiles' }),
+	new IconComponent({ name: 'Текстовый файл 4', img: 'text-file', entry: '#myFiles' }),
+	new IconComponent({ name: 'Видео-файл 1', img: 'movie', entry: '#myFiles' }),
+	new IconComponent({ name: 'Видео-файл 2', img: 'movie', entry: '#myFiles' }),
+	new IconComponent({ name: 'Видео-файл 3', img: 'movie', entry: '#myFiles' }),
+	new IconComponent({ name: 'Видео-файл 4', img: 'movie', entry: '#myFiles' }),
+];
+myFiles.files = [
+	new IconComponent({ name: 'Текстовый файл 1', img: 'text-file', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Текстовый файл 2', img: 'text-file', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Текстовый файл 3', img: 'text-file', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Текстовый файл 4', img: 'text-file', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Текстовый файл 5', img: 'text-file', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Текстовый файл 6', img: 'text-file', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Видео-файл 1', img: 'movie', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Видео-файл 2', img: 'movie', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Видео-файл 3', img: 'movie', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Видео-файл 4', img: 'movie', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Видео-файл 5', img: 'movie', entry: '#manyFiles' }),
+	new IconComponent({ name: 'Видео-файл 6', img: 'movie', entry: '#manyFiles' }),
 ];
 
 // Window close engine
 myFiles.closeIt();
 anotherFiles.closeIt();
+myFiles.closeIt();
--- src/js/components/DragNDrop.js
+++ src/js/components/DragNDrop.js
@@ -1,68 +1,68 @@
-document.onmousedown = function(e) {
+var DragNDrop = (function() {
 
-  if (!e.target.classList.contains('draggable')) return;
+  var app = document.querySelector('#app');
 
-  var coords, shiftX, shiftY;
+  app.onmousedown = function(e) {
+    if (!e.target.classList.contains('draggable')) return;
+    var toX, toY;
+    function startDrag(clientX, clientY) {
+      e.target.style.position = 'fixed';
+      toX = clientX - e.target.getBoundingClientRect().left;
+      toY = clientY - e.target.getBoundingClientRect().top;
+      app.appendChild(e.target);
+      moveTo(clientX, clientY);
+    };
 
-  startDrag(e.clientX, e.clientY);
+    function finishDrag() {
+      e.target.style.top = parseInt(e.target.style.top) + pageYOffset + 'px';
+      e.target.style.position = 'absolute';
+      app.onmousemove = null;
+      e.target.onmouseup = null;
+    }
 
-  document.onmousemove = function(e) {
-    moveAt(e.clientX, e.clientY);
-  };
-
-  e.target.onmouseup = function() {
-    finishDrag();
-  };
-
-
-  function startDrag(clientX, clientY) {
-    e.target.style.position = 'fixed';
-    shiftX = clientX - e.target.getBoundingClientRect().left;
-    shiftY = clientY - e.target.getBoundingClientRect().top;
-    document.body.appendChild(e.target);
-    moveAt(clientX, clientY);
-  };
-
-  function finishDrag() {
-    e.target.style.top = parseInt(e.target.style.top) + pageYOffset + 'px';
-    e.target.style.position = 'absolute';
-    document.onmousemove = null;
-    e.target.onmouseup = null;
-  }
-
-  function moveAt(clientX, clientY) {
-    var X = clientX - shiftX;
-    var Y = clientY - shiftY;
-    var newBottom = Y + e.target.offsetHeight;
-    if (newBottom > document.documentElement.clientHeight) {
-      var docBottom = document.documentElement.getBoundingClientRect().bottom;
-      var scrollY = Math.min(docBottom - newBottom, 10);
-      if (scrollY < 0) {
-      	scrollY = 0;
+    function moveTo(clientX, clientY) {
+      var X = clientX - toX;
+      var Y = clientY - toY;
+      var newBottom = Y + e.target.offsetHeight;
+      if (newBottom > app.clientHeight) {
+        var docBottom = app.getBoundingClientRect().bottom;
+        var scrollY = Math.min(docBottom - newBottom, 10);
+        if (scrollY < 0) {
+        	scrollY = 0;
+        }
+        window.scrollBy(0, scrollY);
+        Y = Math.min(Y, app.clientHeight - e.target.offsetHeight);
       }
-      window.scrollBy(0, scrollY);
-      Y = Math.min(Y, document.documentElement.clientHeight - e.target.offsetHeight);
+
+      if (Y < 0) {
+        var scrollY = Math.min(-Y, 10);
+        if (scrollY < 0) scrollY = 0; 
+        window.scrollBy(0, -scrollY);
+        Y = Math.max(Y, 0);
+      }
+
+      if (X < 0) {
+        X = 0;
+      }
+
+      if (X > app.clientWidth - e.target.offsetHeight) {
+        X = app.clientWidth - e.target.offsetHeight;
+      }
+
+      e.target.style.left = X + 'px';
+      e.target.style.top = Y + 'px';
     }
+    startDrag(e.clientX, e.clientY);
+
+    app.onmousemove = function(e) {
+      moveTo(e.clientX, e.clientY);
+    };
+
+    e.target.onmouseup = function() {
+      finishDrag();
+    };
 
 
-    if (Y < 0) {
-      var scrollY = Math.min(-Y, 10);
-      if (scrollY < 0) scrollY = 0; // поправим ошибку округления
-      window.scrollBy(0, -scrollY);
-      Y = Math.max(Y, 0);
-    }
 
-    if (X < 0) {
-      X = 0;
-    }
-
-    if (X > document.documentElement.clientWidth - e.target.offsetHeight) {
-      X = document.documentElement.clientWidth - e.target.offsetHeight;
-    }
-
-    e.target.style.left = X + 'px';
-    e.target.style.top = Y + 'px';
   }
-
-
-}
+}())
